10a0c771c86096b44ccc2e6fbc60b0763209a921
codebymojnu <thisismojnu@gmail.com>
2024-03-15 22:41:25 +0600

    change theme design

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import WorkExperienceSection from "./components/WorkExperienceSection";
 
 export default function App() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 w-full md:max-w-4xl  m-2  md:p-8 border border-gray-300 rounded-lg shadow-md">
       <FirstSection />
       <AreaOfInterest />
       <EducationSection />
